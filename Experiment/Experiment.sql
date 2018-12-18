7c20f99107dbd15b6b62a0e4176e8ad653d14b72
Wencong Guan <31893053+Eternity-Myth@users.noreply.github.com>
2018-12-19 00:33:23 +0800

    Update Experiment.sql

--- Experiment/Experiment.sql
+++ Experiment/Experiment.sql
@@ -34,7 +34,7 @@ FROM
 	NATURAL JOIN room_type
 	NATURAL JOIN room_info 
 WHERE
-	date <= '2018-11-16' AND date >= '2018-11-15' 
+	date >= '2018-11-14' AND date <= '2018-11-15' 
 GROUP BY
 	room_id 
 HAVING
@@ -66,4 +66,73 @@ FROM
 	NATURAL JOIN hotel 
 WHERE
 	start_date = '2018-11-14';
+*/
+
+/* Q6
+SELECT
+	hotel_name,
+	date,
+	totalremain,
+	totalprice / totalremain AS avgprice 
+FROM
+	(
+	SELECT
+		hotel_name,
+		date,
+		SUM( remain ) AS totalremain,
+		SUM( price * remain ) AS totalprice 
+	FROM
+		hotel
+		NATURAL JOIN room_type
+		NATURAL JOIN room_info
+	WHERE date>='2018-11-14'AND date<='2018-11-16'	
+	GROUP BY
+		hotel_name,
+		date 
+	) AS r1
+ORDER BY
+	avgprice;
+*/
+
+/* Q2 Select all
+SELECT
+	hotel_name,
+	date,
+	AVG( price ) AS avgprice 
+FROM
+	room_info
+	NATURAL JOIN room_type
+	NATURAL JOIN hotel
+GROUP BY
+	hotel_name,
+	date
+ORDER BY
+	avgprice;
+*/
+
+/* Q3 Select all
+SELECT
+	hotel_name,
+	room_name,
+	avg( price ) AS avgprice 
+FROM
+	hotel
+	NATURAL JOIN room_type
+	NATURAL JOIN room_info 
+GROUP BY
+	room_id 
+ORDER BY
+	avgprice;
+*/
+
+/* Q5 Select all
+SELECT
+	hotel_name,
+	start_date,
+	room_name,
+	amount 
+FROM
+	`order`
+	NATURAL JOIN room_type
+	NATURAL JOIN hotel;
 */
